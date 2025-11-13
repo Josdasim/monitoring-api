7f7d8e454906a0409b40b267313e0cfa74cd69f7
Jose Simanca <josdsim31@gmail.com>
2025-11-13 07:09:26 -0500

    feat: Se actualiza la clase principal que ejecuta el API, añadiendo los nuevos endpoints (#16)

--- src/app/main.py
+++ src/app/main.py
@@ -1,20 +1,44 @@
 from fastapi import FastAPI
-from src.app.api.v1.endpoints import services
+from contextlib import asynccontextmanager
+from src.app.api.v1.endpoints import services, health_checks
+from src.app.services.scheduler_service import health_scheduler
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Maneja el ciclo de vida de la aplicacion"""
+    print("Iniciando Monitoring API...")
+    health_scheduler.start_monitoring(interval_seconds=60)
+    print("API ready")
+
+    yield
+
+    print("Apagando...")
+    health_scheduler.stop_monitoring()
+    print("¡Apagado!")
 
 
 app = FastAPI(
     title = "Monitoring API",
     description = "API para monitorear servicios",
-    version="0.1.0"
+    version="1.0.0",
+    lifespan=lifespan
 )
 
 # Incluir routers
 app.include_router(services.router, prefix="/api/v1")
+app.include_router(health_checks.router, prefix="/api/v1")
 
 @app.get("/")
 def root():
-    return {"message":"Monitoring API is running"}
+    """Endpoint Raiz"""
+    return {
+        "message":"Monitoring API is running",
+        "version":"1.0.0",
+        "docs":"/docs"
+        }
 
 @app.get("/health")
 def health_check():
+    """Health check de la propia API"""
     return {"status":"healthy"}
