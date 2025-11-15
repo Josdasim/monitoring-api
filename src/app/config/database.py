6d80de9bb96856c4024f58e5551f29df7ba71b4d
Jose Simanca <josdsim31@gmail.com>
2025-11-15 07:33:40 -0500

    feat: Se actualiza archivo 'database.py' Para tomar la variable de entorno (#20)

--- src/app/config/database.py
+++ src/app/config/database.py
@@ -1,18 +1,23 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
 
-#De momento se manejara con sqlite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./monitoring.db"
 
-# Motor de la DB
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Configurar conexion con el motor de la DB
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args = {"check_same_thread": False} #Solo para test en sqlite
+    DATABASE_URL,
+    echo = False,
+    pool_pre_ping = True
+
 )
 
-# Crear sesiones
+# Configuracion de creacion de sesiones
 sessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 # Clase para los modelos
